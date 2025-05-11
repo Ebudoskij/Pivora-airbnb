ddf7b88a20af7ece7178c6851f5839d7515cbe50
LoFiSwanL <daiki.ren710@gmai.com>
2025-05-11 15:58:11 +0300

    + fix bug

--- src/main/java/org/ebudoskyi/houserent/repository/PropertyRepository.java
+++ src/main/java/org/ebudoskyi/houserent/repository/PropertyRepository.java
@@ -3,6 +3,8 @@ package org.ebudoskyi.houserent.repository;
 import jakarta.validation.constraints.Positive;
 import org.ebudoskyi.houserent.model.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -10,7 +12,8 @@ import java.util.List;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
-    List<Property> findByOwnerId(Long propertyId, LocalDate availableDate);
+    @Query("SELECT p FROM Property p WHERE p.owner.id = :ownerId AND p.createdDate = :date")
+    List<Property> findByOwnerIdAndDate(@Param("ownerId") Long ownerId, @Param("date") LocalDate date);
     List<Property> findByLocation(String location);
     List<Property> findByLocationContainingIgnoreCase(String location);
     List<Property> findByPricePerNightBetween(double min, double max);
