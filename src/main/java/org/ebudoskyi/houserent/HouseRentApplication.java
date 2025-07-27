3c240430368e53b42f3730ebdb0c279f220f4dc0
Yehor Budovskyi <egorbudovskij@gmail.com>
2025-07-27 19:32:00 +0300

    Set up environment variables with dotenv-java

--- src/main/java/org/ebudoskyi/houserent/HouseRentApplication.java
+++ src/main/java/org/ebudoskyi/houserent/HouseRentApplication.java
@@ -1,5 +1,6 @@
 package org.ebudoskyi.houserent;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class HouseRentApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("API_KEY", dotenv.get("API_KEY"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		SpringApplication.run(HouseRentApplication.class, args);
 	}
 
